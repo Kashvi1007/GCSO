5cd3dfa95bff6a9604315a9fb47ef40a60f8c3bb
Kashvi1007 <68150265+Kashvi1007@users.noreply.github.com>
2020-08-01 18:39:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,9 +16,7 @@ function draw() {
   
   car.velocityX=speed;
 
-  drawSprites();
-}
-if(wall.x-car.x<(car.width+wall.width)/2)
+  if(wall.x-car.x<(car.width+wall.width)/2)
 {
   car.velocityX=0;
   var deformation=0.5* weight *speed* speed/2
@@ -34,4 +32,7 @@ if(wall.x-car.x<(car.width+wall.width)/2)
   {
     car.shapeColor=color(0,255,0);
   }
+}
+
+  drawSprites();
 }
