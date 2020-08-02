3bae23752d10a91a562345bd7bf0a7595e86ae71
Kashvi1007 <68150265+Kashvi1007@users.noreply.github.com>
2020-08-02 13:08:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,10 +7,10 @@ function setup() {
   speed=random(55,90);
   weight=random(400,1500)
 
-  car.velocityX=speed;
-
   car= createSprite(50, 200, 50, 50);
   wall=createSprite(1500,200,60,height/2);
+
+  car.velocityX=speed;
 }
 
 function draw() {
